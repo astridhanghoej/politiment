c3fb6bab2ccc60e1ef58063bc43e72f6c1280947
astridhanghoej <astridhanghoej@gmail.com>
2016-07-06 09:25:15 +0200

    add code for data harvest

---
+++ trainingdataharvest.R
@@ -0,0 +1,23 @@
+#Load packages
+library(twitteR)
+
+#Setup Oauth
+api_key<-"YourApiKeyHere"
+api_secret<-"YourApiSecretHere"
+access_token<-"YourAccessTokenHere"
+access_token_secret<-"YourAccessTokenSecretHere"
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
+#Get twitter data
+happydata<-searchTwitter("dkpol+:)+exclude:retweets+lang:da", n=10000)
+unhappydata<-searchTwitter("dkpol+:(+exclude:retweets+lang:da", n=10000)
+
+#Subset data to be used from list element
+tweetshappy<-as.data.frame(cbind(username=sapply(happydata,function(x) x$screenName),tweet=sapply(happydata,function(x) x$text)))
+tweetsunhappy<-as.data.frame(cbind(username=sapply(unhappydata,function(x) x$screenName),tweet=sapply(unhappydata,function(x) x$text)))
+
+#Combine data into dataframe
+data<-rbind(tweetshappy,tweetsunhappy)
+
+#Append to CSV file
+write.table(data,"trainingdata.csv",append=F,col.names = F,row.names = F)
